c079be7259736f92e6b30b6064832df4f95ed919
Anton <antpavsor@gmail.com>
2020-08-24 02:27:48 +0300

    Реализована двухуровневая архитектура тестов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,126 +1,20 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest
+import pytest
 from model.contact import Contact
+from fixture.application import Application
 
 
-class TestAddContact(unittest.TestCase):
-    def setUp(self):
-        self.driver = webdriver.Firefox()
-        self.driver.implicitly_wait(30)
-        self.base_url = "https://www.google.com/"
-        self.verificationErrors = []
-        self.accept_next_alert = True
-
-    def test_add_contact(self):
-        driver = self.driver
-        self.open_homepage(driver)
-        self.login(driver, username="admin", password="secret")
-        self.open_contact_adding_page(driver)
-        self.create_contact(driver, Contact(firstname="fsdafdsfds", middlename="sdfsdfds", lastname="dfsdfdsf", nickname="sfsfsd", title="sdfsfwf", company="sfsfs", address="xvxvxv", home="xvvx", mobile="sfdfdf", work="sfdsfdsf", fax="sfsfsf", email="sfsfsfsf", email2="sfsffvxv", email3="xvxvxv", homepage="cbcbgg"))
-        self.open_contacts_page(driver)
-        self.logout(driver)
-
-    def create_contact(self, driver, contact):
-        driver = self.driver
-        driver.find_element_by_name("firstname").click()
-        driver.find_element_by_name("firstname").clear()
-        driver.find_element_by_name("firstname").send_keys(contact.firstname)
-        driver.find_element_by_name("middlename").click()
-        driver.find_element_by_name("middlename").clear()
-        driver.find_element_by_name("middlename").send_keys(contact.middlename)
-        driver.find_element_by_name("lastname").click()
-        driver.find_element_by_name("lastname").clear()
-        driver.find_element_by_name("lastname").send_keys(contact.lastname)
-        driver.find_element_by_name("nickname").click()
-        driver.find_element_by_name("nickname").clear()
-        driver.find_element_by_name("nickname").send_keys(contact.nickname)
-        driver.find_element_by_name("title").click()
-        driver.find_element_by_name("title").clear()
-        driver.find_element_by_name("title").send_keys(contact.title)
-        driver.find_element_by_name("company").click()
-        driver.find_element_by_name("company").clear()
-        driver.find_element_by_name("company").send_keys(contact.company)
-        driver.find_element_by_name("address").click()
-        driver.find_element_by_name("address").clear()
-        driver.find_element_by_name("address").send_keys(contact.address)
-        driver.find_element_by_name("home").click()
-        driver.find_element_by_name("home").clear()
-        driver.find_element_by_name("home").send_keys(contact.home)
-        driver.find_element_by_name("mobile").clear()
-        driver.find_element_by_name("mobile").click()
-        driver.find_element_by_name("mobile").send_keys(contact.mobile)
-        driver.find_element_by_name("work").click()
-        driver.find_element_by_name("work").clear()
-        driver.find_element_by_name("work").send_keys(contact.work)
-        driver.find_element_by_name("fax").click()
-        driver.find_element_by_name("fax").clear()
-        driver.find_element_by_name("fax").send_keys(contact.fax)
-        driver.find_element_by_name("email").click()
-        driver.find_element_by_name("email").clear()
-        driver.find_element_by_name("email").send_keys(contact.email)
-        driver.find_element_by_name("email2").click()
-        driver.find_element_by_name("email2").clear()
-        driver.find_element_by_name("email2").send_keys(contact.email2)
-        driver.find_element_by_name("email3").click()
-        driver.find_element_by_name("email3").clear()
-        driver.find_element_by_name("email3").send_keys(contact.email3)
-        driver.find_element_by_name("homepage").click()
-        driver.find_element_by_name("homepage").clear()
-        driver.find_element_by_name("homepage").send_keys(contact.homepage)
-        driver.find_element_by_name("submit").click()
-
-    def logout(self, driver):
-        driver.find_element_by_link_text("Logout").click()
-
-    def open_contacts_page(self, driver):
-        driver.find_element_by_link_text("home").click()
-
-    def open_contact_adding_page(self, driver):
-        driver.find_element_by_link_text("add new").click()
-
-    def login(self, driver, username, password):
-        driver.find_element_by_name("user").clear()
-        driver.find_element_by_name("user").send_keys(username)
-        driver.find_element_by_name("pass").clear()
-        driver.find_element_by_name("pass").send_keys(password)
-        driver.find_element_by_xpath("//input[@value='Login']").click()
-
-    def open_homepage(self, driver):
-        driver.get("http://localhost/addressbook/")
-
-    def is_element_present(self, how, what):
-        try:
-            self.driver.find_element(by=how, value=what)
-        except NoSuchElementException as e:
-            return False
-        return True
-
-    def is_alert_present(self):
-        try:
-            self.driver.switch_to_alert()
-        except NoAlertPresentException as e:
-            return False
-        return True
-
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.driver.switch_to_alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally:
-            self.accept_next_alert = True
-
-    def tearDown(self):
-        self.driver.quit()
-        self.assertEqual([], self.verificationErrors)
+@pytest.fixture()
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_add_contact(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.create(
+        Contact(firstname="fsdafdsfds", middlename="sdfsdfds", lastname="dfsdfdsf", nickname="sfsfsd", title="sdfsfwf",
+                company="sfsfs", address="xvxvxv", home="xvvx", mobile="sfdfdf", work="sfdsfdsf", fax="sfsfsf",
+                email="sfsfsfsf", email2="sfsffvxv", email3="xvxvxv", homepage="cbcbgg"))
+    app.session.logout()
