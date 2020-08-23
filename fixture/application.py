669c83ebe293b5cb2c6b030ea13403ee8ea4552e
Anton <antpavsor@gmail.com>
2020-08-24 02:14:40 +0300

    Выделен класс для управления группами

--- fixture/application.py
+++ fixture/application.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from fixture.session import SessionHelper
+from fixture.group import GroupHelper
+
 
 class Application:
 
@@ -7,38 +9,11 @@ class Application:
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
         self.session = SessionHelper(self)
-
-    def return_to_groups_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("group page").click()
-
-    def create_group(self, group):
-        wd = self.wd
-        self.open_groups_page()
-        # Init Group Creation
-        wd.find_element_by_name("new").click()
-        # Fill Group Form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # Submit Group Creation
-        wd.find_element_by_name("submit").click()
-        self.return_to_groups_page()
-
-    def open_groups_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("groups").click()
+        self.group = GroupHelper(self)
 
     def open_homepage(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/")
-
 
     def destroy(self):
         self.wd.quit()
