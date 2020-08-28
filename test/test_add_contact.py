ba74de5c1b91136df064ea4126927a9aad055c2e
Anton <antpavsor@gmail.com>
2020-08-28 19:13:51 +0300

    Логин и логаут вынесены в фикстуру

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,9 +3,7 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(
         Contact(firstname="fsdafdsfds", middlename="sdfsdfds", lastname="dfsdfdsf", nickname="sfsfsd", title="sdfsfwf",
                 company="sfsfs", address="xvxvxv", home="xvvx", mobile="sfdfdf", work="sfdsfdsf", fax="sfsfsf",
                 email="sfsfsfsf", email2="sfsffvxv", email3="xvxvxv", homepage="cbcbgg"))
-    app.session.logout()
--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -3,9 +3,7 @@ from model.contact import Contact
 
 
 def test_edit_first_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.edit_first_contact(
         Contact(firstname="111", middlename="222", lastname="333", nickname="444", title="555",
                 company="666", address="777", home="888", mobile="999", work="1010", fax="1111",
                 email="1212", email2="1313", email3="1414", homepage="1515"))
-    app.session.logout()
