8a8238fe99341f79de6de6eead023a80a3d37638
Anton <antpavsor@gmail.com>
2020-09-17 17:16:32 +0300

    Параметризованы тесты на добавление групп и контактов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,13 +1,27 @@
 from model.contact import Contact
+import pytest
+import random
+import string
 
+def random_string (prefix, maxlen):
+    symbols = string.ascii_letters + string.digits
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
-def test_add_contact(app):
+testdata = [Contact(firstname=random_string("firstname", 10), middlename=random_string("middlename", 10),
+            lastname=random_string("lastname", 10), nickname=random_string("nickname", 10),
+            title=random_string("title", 10), company=random_string("company", 10),
+            address=random_string("address", 40), home=random_string("home", 10),
+            mobile=random_string("mobile", 10), work=random_string("work", 10),
+            fax=random_string("fax", 10), email=random_string("email", 10),
+            email2=random_string("email2", 10), email3=random_string("email3", 10),
+            homepage=random_string("homepage", 10))
+    for i in range(5)]
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="fsdafdsfds", middlename="sdfsdfds", lastname="dfsdfdsf", nickname="sfsfsd",
-                      title="sdfsfwf", company="sfsfs", address="xvxvxv", home="xvvx", mobile="sfdfdf", work="sfdsfdsf",
-                      fax="sfsfsf", email="sfsfsfsf", email2="sfsffvxv", email3="xvxvxv", homepage="cbcbgg")
     app.contact.create(contact)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) + 1 == len(new_contacts)
     old_contacts.append(contact)
-    assert old_contacts == new_contacts
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
