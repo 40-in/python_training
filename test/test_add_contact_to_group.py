aa26ce6aa8968d82a05edc0a216758187cb34425
Anton <antpavsor@gmail.com>
2020-10-04 20:31:53 +0300

    Исправлен тест на добавление контакта в группу

--- test/test_add_contact_to_group.py
+++ test/test_add_contact_to_group.py
@@ -15,14 +15,27 @@ def test_add_contact_to_group(app, db):
     contacts = db.get_contact_list()
     groups = db.get_group_list()
 
-    while True:
-        contact_index = randrange(len(contacts))
-        group_index = randrange(len(groups))
-        contact_id = contacts[contact_index].id
-        group_id = groups[group_index].id
-        if contacts[contact_index] not in orm.get_contacts_in_group(groups[group_index]):
+    pair_found = False
+
+    for C in range(len(contacts)):
+        for G in range(len(groups)):
+            if contacts[C] not in orm.get_contacts_in_group(groups[G]):
+                pair_found = True
+                contact_id = contacts[C].id
+                group_id = groups[G].id
+                break
+        if pair_found:
             break
+
+    if not pair_found:
+        app.group.create(Group(name="test"))
+        groups = db.get_group_list()
+        G = -1
+        group_id = groups[G].id
+        C = randrange(len(contacts))
+        contact_id = contacts[C].id
+        app.contact.open_contacts_page()
 
     app.contact.add_contact_to_group(contact_id, group_id)
 
-    assert contacts[contact_index] in orm.get_contacts_in_group(groups[group_index])
+    assert contacts[C] in orm.get_contacts_in_group(groups[G])
